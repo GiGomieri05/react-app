9912ba47cb13c58204515cb455224666d3d5691c
GiGomieri05 <giovannidelucagomieri@hotmail.com>
2026-04-08 22:07:21 -0300

    implement click counter function

--- src/components/ClickCounter.jsx
+++ src/components/ClickCounter.jsx
@@ -1,8 +1,13 @@
+import { useState } from 'react';
+
 function ClickCounter() {
+  const [count, setCount] = useState(0); // initial value: 0
+
   return (
     <div>
       <h2>Conta Clique</h2>
-      <p>Conteúdo</p>
+      <p> Me clicaram {count} vezes</p>
+      <button onClick={() => setCount(count + 1)}>Clique aqui!</button>
     </div>
   );
 }
